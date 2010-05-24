fb13550a020f424d27589d1ed1e4318a185138be
Gerolf Seitz <gerolf.seitz@gmail.com>
2010-05-24 02:11:59 +0200

    bad tabs

--- src/main/scala/CassandraDataStore.scala
+++ src/main/scala/CassandraDataStore.scala
@@ -16,55 +16,55 @@ import se.scalablesolutions.akka.persistence.common._
  *
  */
 class CassandraDataStore(val applicationName: String, hostname: String, port: Int) extends IDataStore {
-	
-	private val sessions = new CassandraSessionPool(
-			getKeyspace,
-			StackPool(SocketProvider(hostname, port)),
-			Protocol.Binary,
-			2)
-	
-	implicit def intToByteArray(i: Int) : Array[Byte] = String.valueOf(i).getBytes
-	
-	def |<(k: String) = applicationName + "_" + k
-	
-	override def getData(sessionId: String, id: Int) : Array[Byte] = {
-		sessions.withSession { session =>
-			try {
-				val option = session | (|<(sessionId), new ColumnPath(getColumnFamily, null, id))
-				val col = option.get.getColumn
-				col.getValue
-			} catch {
-				case e => null
-			}
-		}
-	}
+  
+  private val sessions = new CassandraSessionPool(
+      getKeyspace,
+      StackPool(SocketProvider(hostname, port)),
+      Protocol.Binary,
+      2)
+  
+  implicit def intToByteArray(i: Int) : Array[Byte] = String.valueOf(i).getBytes
+  
+  def |<(k: String) = applicationName + "_" + k
+  
+  override def getData(sessionId: String, id: Int) : Array[Byte] = {
+    sessions.withSession { session =>
+      try {
+        val option = session | (|<(sessionId), new ColumnPath(getColumnFamily, null, id))
+        val col = option.get.getColumn
+        col.getValue
+      } catch {
+        case e => null
+      }
+    }
+  }
 
-	override def removeData(sessionId: String, id: Int) = {
-		sessions.withSession { session =>
-			session -- ( |<(sessionId), new ColumnPath(getColumnFamily, null, id), System.currentTimeMillis)
-		}
-	}
+  override def removeData(sessionId: String, id: Int) = {
+    sessions.withSession { session =>
+      session -- ( |<(sessionId), new ColumnPath(getColumnFamily, null, id), System.currentTimeMillis)
+    }
+  }
 
-	override def removeData(sessionId: String) = {
-		sessions.withSession { session =>
-			session -- ( |<(sessionId), new ColumnPath(getColumnFamily, null, null), System.currentTimeMillis)
-		}
-	}
+  override def removeData(sessionId: String) = {
+    sessions.withSession { session =>
+      session -- ( |<(sessionId), new ColumnPath(getColumnFamily, null, null), System.currentTimeMillis)
+    }
+  }
 
-	override def storeData(sessionId: String, id: Int, data: Array[Byte]) = {
-		sessions.withSession { session =>
-			session ++| ( |<(sessionId), new ColumnPath(getColumnFamily, null, id), data, System.currentTimeMillis)
-		}
-	}
+  override def storeData(sessionId: String, id: Int, data: Array[Byte]) = {
+    sessions.withSession { session =>
+      session ++| ( |<(sessionId), new ColumnPath(getColumnFamily, null, id), data, System.currentTimeMillis)
+    }
+  }
 
-	override def destroy = {
-		sessions.close
-	}
+  override def destroy = {
+    sessions.close
+  }
 
-	override def isReplicated = true
-	
-	def getKeyspace = "wicket"
-	
-	def getColumnFamily = "Session"
+  override def isReplicated = true
+  
+  def getKeyspace = "wicket"
+  
+  def getColumnFamily = "Session"
 
 }
